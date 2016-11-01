a06e752f051f1fc149daea2b6977f7890134c91c
adilenel <adilene.lopez.2018@gmail.com>
2016-11-01 18:13:13 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,26 @@
+// write your code below!
+
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19){
+    return 'You are a teenager!'
+  } else {
+    return undefined;
+  }
+
+}
+
+function teenager(age) {
+
+}
+
+function ageChecker(age) {
+
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
